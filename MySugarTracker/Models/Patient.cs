ffae7670c85da1efee311e91511b4dce0f656e90
justindlangley <justindlangley@gmail.com>
2015-04-29 10:13:58 -0400

    Created classes and attributes

---
+++ MySugarTracker/Models/Patient.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MySugarTracker.Models
+{
+    public class Patient : User
+    {
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode=true)]
+        public int BirthDate { get; set; }
+        
+
+        public bool? CVD { get; set; }
+        public bool? HBP { get; set; }
+        public bool? Thyroid { get; set;}
+        public bool? Female { get; set;}
+        public bool? Pregnant { get; set; }
+       
+        //Does patient prefer to receive notifications and reminders from email or SMS, or both.
+        public bool EmailPref {get; set;}
+        public bool SMSpref { get; set; }
+
+        //public int Age {get; {return (DateTime.Now - BirthDate)}} 
+
+        //Use Height and weight to calculate BMI
+        public int Weight { get; set;}
+        public int Height { get; set; }
+        public int BMI {get; set; }
+
+
+    }
+}
---
+++ MySugarTracker/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MySugarTracker.Models
+{
+    public class User
+    {
+        [StringLength(50, ErrorMessage = "First name cannot be longer than 50 characters")]
+        public string FirstName { get; set; }
+        [StringLength(50, ErrorMessage = "Last name cannot be longer than 50 characters")]
+        public string LastName { get; set; }
+        [Required(ErrorMessage="You must enter a valid email.")]
+        [EmailAddress]
+        public string Email { get; set; }
+        public int PhoneNumber { get; set; }
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+    }
+}
