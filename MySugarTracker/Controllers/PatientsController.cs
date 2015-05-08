7c4036149c4797106d26508fac01894f4fec1f42
MarkJensen55 <markjensen@tds.net>
2015-05-08 11:48:09 -0400

    Add Navigation Controller

--- MySugarTracker/Controllers/PatientsController.cs
+++ MySugarTracker/Controllers/PatientsController.cs
@@ -4,9 +4,13 @@ using System.Data;
 using System.Data.Entity;
 using System.Linq;
 using System.Net;
+using System.Security.Claims;
 using System.Web;
 using System.Web.Mvc;
 using MySugarTracker.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
 
 namespace MySugarTracker.Controllers
 {
