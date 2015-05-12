44f3417005027e9c45b9da5130fe460e6a8600ba
justindlangley <justindlangley@gmail.com>
2015-05-12 08:53:56 -0400

    Dr Patient List

--- MySugarTracker/ViewModels/DoctorPatient.cs
+++ MySugarTracker/ViewModels/DoctorPatient.cs
@@ -14,5 +14,10 @@ namespace MySugarTracker.ViewModels
         public string PhoneNumber { get; set; }
 
         public string Email { get; set; }
-    }
+
+        myPatients = new List<PatientUser>();
+
+
+
+     }
 }
